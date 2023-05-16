e2269f1e88feb909143edf30aaeca605905adddd
Duhoon <eclip6@naver.com>
2023-05-17 00:45:53 +0900

    fix: Workspace Clean

--- contract/governance/src/community/mod.rs
+++ contract/governance/src/community/mod.rs
@@ -1,3 +1,4 @@
+
 /*!
 Fungible Token implementation with JSON serialization.
 NOTES:
@@ -20,18 +21,28 @@ use near_contract_standards::fungible_token::metadata::{
 };
 use near_contract_standards::fungible_token::FungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::LazyOption;
+use near_sdk::collections::{LazyOption, LookupMap};
 use near_sdk::json_types::U128;
 use near_sdk::{
     env, log, near_bindgen, require, AccountId, Balance, BorshStorageKey, PanicOnDefault,
-    PromiseOrValue,
+    PromiseOrValue, ext_contract
 };
+
+/* NFT */
+use near_contract_standards::non_fungible_token::metadata::{
+    NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
+};
+use near_contract_standards::non_fungible_token::NonFungibleToken;
+use near_contract_standards::non_fungible_token::{Token, TokenId};
+
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     token: FungibleToken,
     metadata: LazyOption<FungibleTokenMetadata>,
+    deposits: LookupMap<AccountId, Balance>,
+    chapter: u16
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -72,6 +83,8 @@ impl Contract {
         let mut this = Self {
             token: FungibleToken::new(StorageKey::FungibleToken),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            deposits: LookupMap::new(b"d"),
+            chapter: 0,
         };
         this.token.internal_register_account(&owner_id);
         this.token.internal_deposit(&owner_id, total_supply.into());
@@ -89,6 +102,28 @@ impl Contract {
 
 near_contract_standards::impl_fungible_token_core!(Contract, token, on_tokens_burned);
 near_contract_standards::impl_fungible_token_storage!(Contract, token, on_account_closed);
+
+#[near_bindgen]
+impl Contract{
+    pub fn add_liquidation(&mut self, amount: U128){
+        let amount = amount.0;
+        require!(amount > 0, "Governanace:amount should be larger than 0");
+        let account_id = env::predecessor_account_id();
+        let balance = self.token.accounts.get(&account_id);
+        require!(balance >= Some(u128::from(amount)), "Governance: Balance should be larger than amount");
+
+        self.deposits.insert(&account_id, &amount);
+    }
+
+    pub fn remove_liquidtaion(&mut self){
+        let account_id = env::predecessor_account_id();
+        self.deposits.remove(&account_id);
+    }
+
+    pub fn reward(&mut self){
+        // 투표에 참여한 만큼. 보상
+    }
+}
 
 #[near_bindgen]
 impl FungibleTokenMetadataProvider for Contract {
--- contract/governance/vote/src/lib.rs
+++ contract/governance/vote/src/lib.rs
@@ -1,3 +1,5 @@
+mod vote;
+
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::{U128, U64};
 use near_sdk::{env, near_bindgen, AccountId, Balance, EpochHeight, Gas, Promise, PromiseError, PanicOnDefault};
@@ -5,71 +7,28 @@ use near_sdk::ext_contract;
 use near_sdk::collections::LookupMap;
 use near_contract_standards::fungible_token::core::ext_ft_core;
 
-type WrappedTimestamp = U64;
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct VoteManager{
+    communities: Vec<AccountId>,
+    is_voting: Vec<bool>,
+}
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
-pub struct Vote{
-    votes: LookupMap<Account, Balance>,
-    total_voted_stake: Balance,
-    result: Option<WrappedTimestamp>,
-    last_epoch_height: EpochHeight,
-    governanace_accountId: AccountId,
-}
-
-impl Default for VotingContract{
-    fn default() -> Self {
-        env::panic(b"Voting Contract should be initialized before usage");
-    }
-}
-
-#[ext_contract(ext_vote)]
-pub trait extVote{
-    pub fn ping(&mut self);
-    pub fn vote(&mut self, is_vote: bool);
-    pub fn get_result(&self) -> Option<WrappedTimestamp>;
-    pub fn get_total_voted_stake(&self) -> (U128, U128);
-    pub fn get_votes(&self) -> LookupMap<AccountId, U128>;
-}
-
-
-#[near_bindgen]
-impl Vote {
+impl VoteManger{
     #[init]
-    pub fn new() -> Self{
-        assert!(!env:state_exists(), "The contract is already initialized");
-        VotingContract {
-            votes: LookupMap::new(b"d"),
-            total_voted_stake: 0,
-            result: None,
-            last_epoch_height: 0,
+    pub fn new()->Self{
+        Self{
+            communities: Vec::new(),
+            is_voting: Vec::new(),
         }
     }
 
-    pub fn ping(&mut self){
-
+    pub fn is_voting(&self, community_id: U128) -> bool{
+        is_voting[u128(community_id.0)]
     }
 
-    pub fn vote(&mut self, is_vote: bool){
-        self.ping();
-        if self.result.is_some(){
-            return;
-        }
-        let account_id = env::predecessor_account_id();
-        ext_ft_core::ft_balance_of(self.governanace_accountId.copy())
-        .with_static_gas(Gas)
-        ;
-    }
-
-    pub fn get_result(&self) -> Option<WrappedTimestamp>{
-
-    }
-
-    pub fn get_total_voted_stake(&self) -> (U128, U128){
-
-    }
-
-    pub fn get_votes(&self) -> LookupMap<AccountId, U128>{
-
+    pub fn get_community(&self, community_id: U128) -> bool{
+        communities[u128(community_id.0)]
     }
 }
